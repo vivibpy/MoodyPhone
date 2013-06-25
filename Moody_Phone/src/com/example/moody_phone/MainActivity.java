3176368b72776f2cb09e3e12ffca24e20af9535b
Vivian Poetzscher <vivibpy@gmail.com>
2013-06-25 20:18:41 -0300

    Moody_Phone
    sem R T.T

--- Moody_Phone/src/com/example/moody_phone/MainActivity.java
+++ Moody_Phone/src/com/example/moody_phone/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.moody_phone;
 
-import android.os.Bundle;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +21,14 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	public void onClickTemas(View View){
+		
+	}
+	public void onClickPersonalizados(View View){
+		
+	}
+	public void onClickConfig(View View){
+	
+}
 }
